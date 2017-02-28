432c6738d6bcad46eec9b8a8c2f2e9172bc7c92c
gavrilovaa82@rambler.ru <gavrilovaa82@rambler.ru>
2017-02-28 19:26:15 +0600

    my project

--- myproject/index.php
+++ myproject/index.php
@@ -1,2 +1,17 @@
 <?php
 echo 'test';
+
+
+
+function massiv($name, $tel, $address)
+{
+    return $massiv = array ($name, $tel, $address);
+    
+}
+$name = $_GET["Name1"];
+$tel = $_GET["Tel1"];
+$address = $_GET["Address1"];
+$res = massiv ($name, $tel, $address);
+
+foreach ($res as $k => $v)
+    echo "<br>$k => $v";
