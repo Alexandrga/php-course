deade9d4fd1e78339e7eac41af4afcbfe4813bca
gavrilovaa82@rambler.ru <gavrilovaa82@rambler.ru>
2017-03-04 15:17:43 +0600

    php

--- myproject/index.php
+++ myproject/index.php
@@ -5,10 +5,11 @@ echo 'test';
 
 $text = $_GET["text"];
 $file = $_GET["File"];
-
-
-$file = fopen ($file, "w+");
+$file = fopen ($file, "wb");
+for ($i=1; $i<50; ++$i)
+{
 fwrite ($file, $text);
+}
 
 
 
