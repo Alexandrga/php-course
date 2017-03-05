f04527c3741c752ab54684ae39acf7e8c00d444d
gavrilovaa82@rambler.ru <gavrilovaa82@rambler.ru>
2017-03-05 20:27:43 +0600

    home task

--- myproject/main.php
+++ myproject/main.php
@@ -27,7 +27,9 @@ while (($buffer=fgets($file, 4096)) != false) {
         <td><?php echo $list[0][3]?></td>
         <td><?php echo $list[0][4]?></td>
     </tr>
-    <tr><?php for ($i=1; $i<count($list); $i++):?>
+    <tr><?php for ($i=1; $i<count($list); $i++):
+            
+        // for ($k=0; $k<count($row); $k++):?>
         <td> <?php echo $list[$i][0];?></td>
         <td><?php echo $list[$i][1];?></td>
         <td><?php echo $list[$i][2];?></td>
@@ -35,7 +37,7 @@ while (($buffer=fgets($file, 4096)) != false) {
         <td><?php echo $list[$i][4];endfor;?></td>
     </tr>
 </table>
-    <
+    
 </form>
     
 
