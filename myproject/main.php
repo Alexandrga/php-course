bb0466471119ecf5b111ae3790c1cf6c82af775f
gavrilovaa82@rambler.ru <gavrilovaa82@rambler.ru>
2017-03-05 20:45:26 +0600

    table

--- myproject/main.php
+++ myproject/main.php
@@ -20,16 +20,11 @@ while (($buffer=fgets($file, 4096)) != false) {
 <input type="text" name="parametr" >
 <input type="submit">
 <table>
+    <?php for ($i=0; $i<count($list); $i++):?>
+    
     <tr>
-        <td><?php echo $list[0][0]?></td>
-        <td><?php echo $list[0][1]?></td>
-        <td><?php echo $list[0][2]?></td>
-        <td><?php echo $list[0][3]?></td>
-        <td><?php echo $list[0][4]?></td>
-    </tr>
-    <tr><?php for ($i=1; $i<count($list); $i++):
             
-        // for ($k=0; $k<count($row); $k++):?>
+    
         <td> <?php echo $list[$i][0];?></td>
         <td><?php echo $list[$i][1];?></td>
         <td><?php echo $list[$i][2];?></td>
