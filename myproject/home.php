a6930dd27998e430750781217ca08d5af6c3122a
LiubovSheyda <liubovsheyda@gmail.com>
2017-03-06 20:04:55 +0600

    tasks-1

--- myproject/home.php
+++ myproject/home.php
@@ -69,6 +69,7 @@ $procent = ($sum*100)/$i;
 echo "Процент от общего числа билетов:".$procent."%<br>";
 */
 echo "Массивы<br>";
+echo "1.<br>";
 $n=9;
 $mas = array();
 for ($i=0; $i<=$n; $i++)
@@ -82,5 +83,54 @@ for ($i=0; $i<=$n; $i++)
             $mas[$i] = 1;
             echo $mas[$i];
 }
+
+echo "<br>2.<br>";
+
+for ($i=0; $i<=10; $i++)
+{
+    $massiv[$i] = $i*$i;  
+
+}
+foreach ($massiv as $key => $value) {
+    echo "$value &nbsp";
+}
+
+echo "<br>3.<br>";
+
+$massiv = array (5,10,3);
+
+echo "Произведение занчений массива равно: ".array_product($massiv). "<br>";
+echo "Сумма значений массива равна:".array_sum($massiv)."<br>";
+$proizv = 1;
+for ($i=0; $i<=2; $i++)
+{
+    $proizv = $proizv*$massiv[$i];
+}
+    echo "Произведение без функции равно: $proizv<br>";
+$sum = 0;
+for ($i=0; $i<=2; $i++)
+{
+    $sum = $sum+$massiv[$i];
+}
+    echo "Сумма без функции равна: $sum<br>";
+
+    echo "<br>3.<br>";
+
+    $massiv = array(5,10,3,2,1,5,6,10);
+
+    for ($i=0; $i<=9; $i++)
+    {
+            if ($massiv [$i] == $massiv[$i+1])
+            {
+                    ;
+
+            }
+            else
+            {
+                echo "В массиве нет повторяющихся элементов";
+            }
+
+
+    }
 
 
